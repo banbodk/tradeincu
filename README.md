f56305416f137758a9177832680fda6ff98e45cd
Ding <dingdfdfdf4@gmail.com>
2013-11-07 15:06:57 -0500

    begin

---
+++ README.md
@@ -0,0 +1,5 @@
+TradeInCU
+
+This is TradeInCU for Columbia University students to make money and Save money by trading second hand goods.
+
+This is a history.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tradeincu::Application.config.secret_key_base = '5afc58a2b0ef852f221bb6edb4b55b4a63a850665a4c8c54fd5bdde543feffca4872d14af413625eebce7c404310a7323065d9ed70a46c3b6743ef16883b7bc2'
