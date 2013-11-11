40cf2ea5824d1cf5016730baa9c26ca2929ec7a0
Ding <dingdfdfdf4@gmail.com>
2013-11-11 16:14:21 -0500

    add header and footer and befor adding columns to users

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
 Tradeincu::Application.config.secret_key_base = '5afc58a2b0ef852f221bb6edb4b55b4a63a850665a4c8c54fd5bdde543feffca4872d14af413625eebce7c404310a7323065d9ed70a46c3b6743ef16883b7bc2'
