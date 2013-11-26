f198b21c523bf46b5dd13a86314aed341d790d2e
Ding <dingdfdfdf4@gmail.com>
2013-11-26 16:59:59 -0500

    before heroku

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ActiveRecord::Base
 	default_scope -> { order('created_at DESC')}
 	validates :user_id, presence: true
 	validates :content, presence: true, length: {maximum: 2000}
-	# validates :name, presence: true, length: {maximum: 100}
-	# validates :price, presence: true, length: {maximum: 10}
+	validates :name, presence: true, length: {maximum: 100}
+	validates :price, presence: true, length: {maximum: 10}
 end
